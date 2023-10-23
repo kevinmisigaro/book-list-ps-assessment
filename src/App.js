a098121bc1b29230bd7bd41151d54ecc9b7946ef
Kevin Misigaro <kevin@databalk.nu>
2023-10-23 13:47:01 +0200

    new commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import DataTable from "react-data-table-component";
+
 
 function App() {
+  const books = [
+    { title: "The Great Gatsby", author: "F. Scott Fitzgerald" },
+    { title: "To Kill a Mockingbird", author: "Harper Lee" },
+    { title: "Harry Potter and the Goblet of Fire", author: "J.K Rowling" },
+  ];
+
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const filteredBooks = books.filter(
+    (book) =>
+      book.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      book.author.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  const columns = [
+    {
+      name: "Title",
+      selector: (row) => row.title,
+      sortable: true,
+    },
+    {
+      name: "Author",
+      selector: (row) => row.author,
+      sortable: true,
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div style={{ margin: "5rem 10rem" }}>
+        <div
+          style={{
+            fontWeight: "bold",
+            marginBottom: "2rem",
+            fontSize: "1.5rem",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Book List
+        </div>
+        <div
+          style={{
+            marginBottom: "2rem",
+          }}
+        >
+          <input
+            type="text"
+            placeholder="Search by title or author"
+            value={searchQuery}
+            style={{
+              padding: "0.5rem 4rem",
+            }}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+        </div>
+        <DataTable columns={columns} data={filteredBooks} />
+      </div>
     </div>
   );
 }
